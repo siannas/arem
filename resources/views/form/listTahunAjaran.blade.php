241c0ae38a414a39a82288ee156084c48fe12073
Ivan Indrakusuma <ivan_indrakusuma@yahoo.com>
2021-09-21 09:41:23 +0700

    Update page, delete jenis pemeriksaan

--- resources/views/form/listTahunAjaran.blade.php
+++ resources/views/form/listTahunAjaran.blade.php
@@ -29,33 +29,29 @@ Validasi
         </button>
       </div>
       <div class="modal-body">
-            <div class="form-group">
-                <label><b>Tahun Ajaran</b></label>
-                <div class="row">
-                    <div class="col">
-                        <input type="text" id="tahun1" class="form-control" placeholder="Tahun 1">
-                    </div>
-                    -
-                    <div class="col">
-                        <input type="text" id="tahun2" class="form-control" placeholder="Tahun 2" disabled>
-                    </div>
+        <div class="form-group">
+            <label><b>Tahun Ajaran</b></label>
+            <div class="row">
+                <div class="col">
+                    <input type="text" id="tahun1" class="form-control" placeholder="Tahun 1" oninput="hitungTahun('tahun1', 'tahun2')">
+                </div>
+                -
+                <div class="col">
+                    <input type="text" id="tahun2" class="form-control" placeholder="Tahun 2" disabled>
                 </div>
             </div>
-            <div class="form-group">
-                <label><b>Jenjang</b></label>
-                <div class="input-group">
-                    <select id="jenjang" class="form-control">
-                        <option selected>Pilih Jenjang</option>
-                        <option value="1">SD</option>
-                        <option value="2">SMP</option>
-                        <option value="3">SMA</option>
-                    </select>
-                </div>
+        </div>
+        <div class="form-group">
+            <label><b>Jenjang</b></label>
+            <div class="input-group">
+                <select id="jenjang" class="form-control">
+                    <option selected disabled>Pilih Jenjang</option>
+                    <option value="1">SD</option>
+                    <option value="2">SMP</option>
+                    <option value="3">SMA</option>
+                </select>
             </div>
-            <div class="custom-control custom-switch mt-4">
-                <input type="checkbox" class="custom-control-input" id="customSwitch1">
-                <label class="custom-control-label" for="customSwitch1">Aktifkan</label>
-            </div>
+        </div>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
@@ -135,13 +131,13 @@ Validasi
                                 <div class="row m-1">
                                     @if($role==='Kota')
                                     <form action="{{route('formulir.show', [$unit->id])}}" method="GET" class="mr-1"><button class="btn btn-sm btn-primary"><i class="fas fa-fw fa-eye"></i></button></form>
-                                    <button type="button" class="btn btn-sm btn-warning mr-1" data-toggle="modal" data-target="#tambah"><i class="fas fa-fw fa-edit"></i></button>
+                                    <button type="button" class="btn btn-sm btn-warning mr-1" data-toggle="modal" data-target="#edit"><i class="fas fa-fw fa-edit"></i></button>
                                     <form action="/formulir/duplicate/{{ $unit->id }}" method="POST" class="mr-1">
                                     @csrf
                                         <button class="btn btn-sm btn-secondary"><i class="fas fa-fw fa-copy"></i></button>
                                     </form>
-                                    <button class="btn btn-sm btn-danger"><i class="fas fa-fw fa-trash-alt" data-toggle="modal" data-target="#deleteModal"></i></button>
-                                    <div class="modal modal-danger fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="Delete" aria-hidden="true">
+                                    <button class="btn btn-sm btn-danger"><i class="fas fa-fw fa-trash-alt" data-toggle="modal" data-target="#delete{{ $unit->id }}"></i></button>
+                                    <div class="modal modal-danger fade" id="delete{{ $unit->id }}" tabindex="-1" role="dialog" aria-labelledby="Delete" aria-hidden="true">
                                         <div class="modal-dialog" role="document">
                                             <div class="modal-content">
                                                 <div class="modal-header">
@@ -171,6 +167,51 @@ Validasi
                                 </div>
                             </td>
                         </tr>
+                        <div class="modal fade" id="edit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Edit Form Skrining</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                    <div class="form-group">
+                                        <label><b>Tahun Ajaran</b></label>
+                                        <div class="row">
+                                            <div class="col">
+                                                <input type="text" id="tahun1{{ $unit->id }}" class="form-control" placeholder="Tahun 1" oninput="hitungTahun('tahun1{{$unit->id}}', 'tahun2{{$unit->id}}')">
+                                            </div>
+                                            -
+                                            <div class="col">
+                                                <input type="text" id="tahun2{{ $unit->id }}" class="form-control" placeholder="Tahun 2" disabled>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label><b>Jenjang</b></label>
+                                        <div class="input-group">
+                                            <select id="jenjang" class="form-control">
+                                                <option selected disabled>Pilih Jenjang</option>
+                                                <option value="1">SD</option>
+                                                <option value="2">SMP</option>
+                                                <option value="3">SMA</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                    <div class="custom-control custom-switch mt-4">
+                                        <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                                        <label class="custom-control-label" for="customSwitch1">Aktifkan</label>
+                                    </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                                <button type="button" class="btn btn-primary">Simpan</button>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
                         @endforeach
                     </tbody>
                 </table>
@@ -184,22 +225,15 @@ Validasi
 
 @section('script')
 <script>
-$(document).ready(function(){
-    $('.tahun1').ready(function(){
-        $(this).keyup(function(){
-            calculateTahun2();
-        });
-    }); 
-});
-
-function calculateTahun2(){
-    var tahun = document.getElementById('tahun1');
+function hitungTahun(id1, id2){
+    console.log(id1)
+    var tahun = document.getElementById(id1);
     var tot = 0;
 
     if(parseInt(tahun.value))
         tot = parseInt(tahun.value)+1;
     
-    document.getElementById('tahun2').value = tot;
+    document.getElementById(id2).value = tot;
 }
 
 </script>
