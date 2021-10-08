cb1473bd59580c1e143e7a459df14c26eb7d21b9
siannas <massive.annas@gmail.com>
2021-10-08 09:05:11 +0700

    login head nambahin kata 'asset'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,13 +12,13 @@
     <title>Aplikasi Remaja</title>
 
     <!-- Custom fonts for this template-->
-    <link href="plugins/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="{{asset('public/plugins/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="{{asset('css/sb-admin-2.min.css')}}" rel="stylesheet">
 
 </head>
 
@@ -79,7 +79,7 @@
 
     </div>
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="{{ asset('js/sb-admin-2.min.js')}}"></script>
 
 </body>
 
